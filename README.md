254b7420fb99872a176130683a4a837e76d8a18c
dumblole <move2234@gmail.com>
2018-07-19 16:23:33 -0400

    Added functionality, help is working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # discord-meguBot-work in progress
+
 A discord bot inspired by the amazing Arch Wizard of the Crimson Wizard Clan, Megumin! From the anime, Konosuba.
+
+## Resources
+
+node js
+
+[megumin explosions gif](https://gfycat.com/DaringUncommonGrosbeak)
+
+[megumin profile image](https://www.deviantart.com/mrtapoz-kun/art/Minimalist-Wallpaper-Megumin-Konosuba-625673584)
--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+'use strict';
+const fs = require('fs');
 const botconfig = require('./botconfig.json');
 const meguminQuotes = require('./megumin-quotes.json');
 const Discord = require('discord.js');
 const tokenFile = require('./token.json');
+const explosionQuotes = require('./explosion.json');
 
 const bot = new Discord.Client({ disableEveryone: true });
 
 bot.on('ready', async () => {
+	console.log(`
+███╗   ███╗███████╗ ██████╗ ██╗   ██╗    ██████╗  ██████╗ ████████╗
+████╗ ████║██╔════╝██╔════╝ ██║   ██║    ██╔══██╗██╔═══██╗╚══██╔══╝
+██╔████╔██║█████╗  ██║  ███╗██║   ██║    ██████╔╝██║   ██║   ██║
+██║╚██╔╝██║██╔══╝  ██║   ██║██║   ██║    ██╔══██╗██║   ██║   ██║
+██║ ╚═╝ ██║███████╗╚██████╔╝╚██████╔╝    ██████╔╝╚██████╔╝   ██║
+╚═╝     ╚═╝╚══════╝ ╚═════╝  ╚═════╝     ╚═════╝  ╚═════╝    ╚═╝
+
+`);
 	console.log(`${bot.user.username} is online!`);
 	bot.user.setGame('with Kazuma || ' + botconfig.prefix + ' help');
 });
 
 bot.on('message', message => {
-	let prefix, messageArray, cmd, args, send, msg;
+	let prefix, messageArray, cmd, args, send, msg, msgEmbed, testObj;
+
+	testObj = { nog: 'what' };
 
 	msg = message.content;
 	prefix = botconfig.prefix;
@@ -28,9 +42,10 @@ bot.on('message', message => {
 	) {
 		return;
 	}
+
 	switch (messageArray[0].toLowerCase()) {
 		case 'ping':
-			message.channel.send('pong');
+			message.channel.send('pong!');
 
 			break;
 
@@ -41,63 +56,63 @@ bot.on('message', message => {
 
 			break;
 		case 'hello':
-			message.channel.send('Hello!');
+			message.channel.send('I am Megumin!');
 
 			break;
 		case 'help':
 			message.channel.send({
 				embed: {
-					color: 3447003,
-					author: {
-						name: bot.user.username,
-						icon_url: bot.user.avatarURL
-					},
-					title: 'This is an embed',
-					url: 'http://google.com',
-					description:
-						'This is a test embed to showcase what they look like and what they can do.',
-					fields: [
-						{
-							name: 'Fields',
-							value:
-								'They can have different fields with small headlines.'
-						},
-						{
-							name: 'Masked links',
-							value:
-								'You can put [masked links](http://google.com) inside of rich embeds.'
-						},
-						{
-							name: 'Markdown',
-							value:
-								'You can put all the *usual* **__Markdown__** inside of them.'
-						}
-					],
-					timestamp: new Date(),
+					title: ':question:__COMMAND LIST__:question:',
+					description: `This is a list of currently available commands. Make sure you use the command prefix '**${prefix}**' before typing in any of these commands`,
+					color: 16077395,
 					footer: {
 						icon_url: bot.user.avatarURL,
-						text: '| made by dumblole'
-					}
+						text: '-= Made by dumblole =-'
+					},
+
+					fields: [
+						{
+							name: 'help',
+							value: 'HEEEEEELLLPPP'
+						},
+						{
+							name: 'ping',
+							value: 'pong!'
+						},
+						{
+							name: 'hello',
+							value: 'Say hello to Megumin <3'
+						},
+						{
+							name: 'quote',
+							value:
+								'Megumin says a random line from her entire script in the anime Konosuba.\n*(There are 403 lines)*'
+						},
+						{
+							name: 'explosion',
+							value:
+								'Megumin readies her explosion spell by chanting... \nAnd when you least expec-***BOOOOOM***'
+						}
+					]
 				}
 			});
 			break;
+		case 'explosion':
+			msgEmbed = new Discord.RichEmbed()
+				.setTitle('hi')
+				.setImage('./megumin-explosion.gif');
+			message.channel.send(
+				`*${explosionQuotes[Math.floor(Math.random() * 6)]}*`
+			);
+			message.channel.send({ files: ['./megumin-explosion.gif'] });
+
+			break;
+
 		default:
 	}
-	// if (msg == 'ping') {
-	// 	message.channel.send('pong');
-	// 	return;
-	// }
-	//
-	// if (msg === `${prefix}quote`) {
-	// 	send(meguminQuotes[Math.floor(Math.random() * 403)]);
-	// 	return;
-	// }
-	// if (msg === `${prefix}hello`) {
-	// 	send('Hello!');
-	// 	return;
-	// }
 });
 
 bot.login(tokenFile.token);
 
 // https://discordapp.com/oauth2/authorize?client_id=468530564689952798&scope=bot
+// https://discordapp.com/oauth2/authorize?client_id=468530564689952798&scope=bot&permissions=2146958591
