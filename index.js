6a07c54c6599e68f71d15b381be4af8e0eaaa7ee
dumblole <move2234@gmail.com>
2018-07-18 21:11:39 -0400

    update

--- index.js
+++ index.js
@@ -11,39 +11,91 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', message => {
-	let prefix, messageArray, cmd, args;
+	let prefix, messageArray, cmd, args, send, msg;
 
+	msg = message.content;
 	prefix = botconfig.prefix;
-	messageArray = message.content.split(' ');
-	cmd = messageArray[0];
-	args = messageArray.slice(1);
-	console.log(cmd + prefix);
+	messageArray = message.content.substring(prefix.length).split(' ');
+
+	console.log(messageArray[0].toLowerCase());
 
 	// Talking to the bot in dm's won't work, also it prevents the bot from
 	// potentially activating itself
-	if (message.author.bot || message.channel.type === 'dm') {
+	if (
+		message.author.bot ||
+		message.channel.type === 'dm' ||
+		!message.content.startsWith(prefix)
+	) {
 		return;
 	}
+	switch (messageArray[0].toLowerCase()) {
+		case 'ping':
+			message.channel.send('pong');
 
-	if (message.content == 'ping') {
-		message.channel.sendMessage('pong');
-		return;
-	}
+			break;
 
-	if (message.content === `${prefix} quote`) {
-		message.channel.sendMessage(
-			meguminQuotes[Math.floor(Math.random() * 403)]
-		);
-		return;
+		case 'quote':
+			message.channel.send(
+				meguminQuotes[Math.floor(Math.random() * 403)]
+			);
+
+			break;
+		case 'hello':
+			message.channel.send('Hello!');
+
+			break;
+		case 'help':
+			message.channel.send({
+				embed: {
+					color: 3447003,
+					author: {
+						name: bot.user.username,
+						icon_url: bot.user.avatarURL
+					},
+					title: 'This is an embed',
+					url: 'http://google.com',
+					description:
+						'This is a test embed to showcase what they look like and what they can do.',
+					fields: [
+						{
+							name: 'Fields',
+							value:
+								'They can have different fields with small headlines.'
+						},
+						{
+							name: 'Masked links',
+							value:
+								'You can put [masked links](http://google.com) inside of rich embeds.'
+						},
+						{
+							name: 'Markdown',
+							value:
+								'You can put all the *usual* **__Markdown__** inside of them.'
+						}
+					],
+					timestamp: new Date(),
+					footer: {
+						icon_url: bot.user.avatarURL,
+						text: '| made by dumblole'
+					}
+				}
+			});
+			break;
+		default:
 	}
-	if (message.content === `${prefix} hello`) {
-		message.channel.send('Hello!');
-		return;
-	}
-	if (message.content === `${prefix} help`) {
-		message.channel.send('ahg!');
-		return;
-	}
+	// if (msg == 'ping') {
+	// 	message.channel.send('pong');
+	// 	return;
+	// }
+	//
+	// if (msg === `${prefix}quote`) {
+	// 	send(meguminQuotes[Math.floor(Math.random() * 403)]);
+	// 	return;
+	// }
+	// if (msg === `${prefix}hello`) {
+	// 	send('Hello!');
+	// 	return;
+	// }
 });
 
 bot.login(tokenFile.token);
